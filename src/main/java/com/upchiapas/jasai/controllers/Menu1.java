2435181ce3b09a632ccf570d817ea9f50b958e4a
Villo29 <jesuruga@gmail.com>
2022-07-18 12:43:32 -0500

    tercer moddit

---
+++ src/main/java/com/upchiapas/jasai/controllers/Menu1.java
@@ -0,0 +1,18 @@
+package com.upchiapas.jasai.controllers;
+
+import com.upchiapas.jasai.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Menu1 {
+
+    @FXML
+    private Button btnatras;
+
+    @FXML
+    void btnatrasOnMouseClicked(MouseEvent event) {
+        HelloApplication.setFXML("MenuMarket", "TU PLAZA VIRTUAL");
+    }
+
+}
---
+++ src/main/java/com/upchiapas/jasai/controllers/Menu2.java
@@ -0,0 +1,26 @@
+package com.upchiapas.jasai.controllers;
+
+import com.upchiapas.jasai.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Menu2 {
+
+    @FXML
+    private Button btnatras;
+
+    @FXML
+    private Button btnubicacion;
+
+    @FXML
+    void btnatrasOnMouseClicked(MouseEvent event) {
+        HelloApplication.setFXML("MenuMarket", "Tu Super Cercas");
+    }
+
+    @FXML
+    void btnubicacionOnMouseClicked(MouseEvent event) {
+
+    }
+
+}
