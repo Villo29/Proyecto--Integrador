41a1a4a64ce54e6c02f2dcdf0025889539c4ec9f
Villo29 <jesuruga@gmail.com>
2022-07-19 15:37:07 -0500

    sexto comimt

--- src/main/java/com/upchiapas/jasai/controllers/MenuRopa3.java
+++ src/main/java/com/upchiapas/jasai/controllers/MenuRopa3.java
@@ -1,4 +1,17 @@
 package com.upchiapas.jasai.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class MenuRopa3 {
+
+    @FXML
+    private Button btnatras;
+
+    @FXML
+    void btnatrasOnMouseClicked(MouseEvent event) {
+
+    }
+
 }
