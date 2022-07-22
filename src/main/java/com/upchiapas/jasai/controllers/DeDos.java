c1e670172f740340945c58fa574764e3db10b707
Villo29 <jesuruga@gmail.com>
2022-07-21 20:35:30 -0500

    COMMIT JUEVES NOCHES

---
+++ src/main/java/com/upchiapas/jasai/controllers/DeDos.java
@@ -0,0 +1,2 @@
+package com.upchiapas.jasai.controllers;public interface DeDos {
+}
---
+++ src/main/java/com/upchiapas/jasai/controllers/Perfil.java
@@ -0,0 +1,17 @@
+package com.upchiapas.jasai.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class Perfil {
+
+    @FXML
+    private Hyperlink hlOne;
+
+    @FXML
+    void handlehlOne(ActionEvent event) {
+
+    }
+
+}
