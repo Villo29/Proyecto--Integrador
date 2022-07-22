32e5823f0a022b75e404c4c9d24319fb7ea3f579
Villo29 <jesuruga@gmail.com>
2022-07-22 01:54:57 -0500

    viernes alas 2

--- src/main/java/com/upchiapas/jasai/controllers/AcercaDe.java
+++ src/main/java/com/upchiapas/jasai/controllers/AcercaDe.java
@@ -1,9 +1,16 @@
 package com.upchiapas.jasai.controllers;
 
 import com.upchiapas.jasai.HelloApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static java.awt.Desktop.getDesktop;
 
 public class AcercaDe {
 
@@ -13,6 +20,18 @@ public class AcercaDe {
     @FXML
     void btnatrasOnMouseClicked(MouseEvent event) {
         HelloApplication.setFXML("Interfaz","TU PLAZA VIRTUAL");
+    }
+
+    @FXML
+    void openLinkFace(ActionEvent event) {
+        try {
+            getDesktop().browse(new URI("https://www.facebook.com/JesuVid.RG"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+
+        }
     }
 
 }
